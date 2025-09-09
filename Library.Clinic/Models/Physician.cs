eb45b8cdf2dc05bf90ffb5e16a57090cb59802b6
Isaiah Rohrer <isaiah.rohrer@gmail.com>
2025-09-09 14:11:25 -0400

    Physician class

---
+++ Library.Clinic/Models/Physician.cs
@@ -0,0 +1,20 @@
+namespace Library.Clinic.Models
+{
+    public class Physician
+    {
+        // Basic Demographics
+        public string? Name { get; set; }
+        public DateOnly? GradDate {  get; set; }
+        public int LicenseNumber { get; set; }
+
+
+        public List<string>? Specializations { get; set; } = new List<string>();
+
+
+        public override string ToString()
+        {
+            return $"{LicenseNumber}. {Name}";
+        }
+
+    }
+}
